dcd5089bc51da6f4842c34aba22a13a822603774
Travis Hoppe <travis.hoppe@gmail.com>
2018-08-02 22:30:59 -0400

    Figured out how to get multi cppns working

--- directions.py
+++ directions.py
@@ -16,46 +16,51 @@ from lucid.misc.tfutil import create_session
 from lucid.optvis.param import cppn
 import lucid.optvis.param as param
 
+
 print ("Loading model")
 model = vision_models.InceptionV1()
 model.load_graphdef()
 
-batch_size = 2
-size_n = 200
+batch_size = 3
 
-#starting_training_steps = 2**10
-#intermediate_training_steps = 2**6
+# TEST: size_n lower and lower training steps still works w/lower quality
 
-starting_training_steps = 2**8
-#intermediate_training_steps = 2**4
+size_n = 100
+starting_training_steps = 2**9
 
-optimizer = tf.train.AdamOptimizer(0.005)
-transforms=[]
+optimizer  = tf.train.AdamOptimizer(0.005)
+transforms = []
 
 save_image_dest = "results/images_direction"
 os.system(f'mkdir -p {save_image_dest}')
 
 ###########################################################################
-pi = np.pi
-dim = 204
-N = 30
-phase = np.random.uniform(0, 2*pi, dim)
-period = np.random.uniform(0, pi/10, dim)
-T = np.linspace(0,2*pi,N).reshape(-1,1)
-Y = np.cos(period*T+phase)
-
 sess = create_session()
 t_size = tf.placeholder_with_default(size_n, [])
+param_f = lambda: tf.concat([cppn(t_size) for _ in range(batch_size)], axis=0)
 
-#param_f = lambda: cppn(t_size, batch=batch_size)
+def render_set(n, channel, train_n):
 
-param_f = lambda: param.image(200, batch=batch_size)
+    neuron1 = (channel, n)
+    neuron2 = (channel, n)
 
-def render_set(n, channel, vec, train_n):
-
-    neuron1 = ("mixed4a_pre_relu", 476)
-    neuron2 = ("mixed4a_pre_relu", 460)
+    # Sum objective on each channel oversaturates
     obj = objectives.channel(*neuron1, batch=0) + objectives.channel(*neuron2, batch=1)
+
+    # This doesn't oversaturate, but images are the same
+    #obj = objectives.channel(*neuron1, batch=0)
+
+    # This does absolutely nothing to mix images (either plus or minus)
+    #obj -= 1e2*objectives.diversity("mixed5a")
+
+    '''
+    interpolation_objective = objectives.channel_interpolate(*neuron1, *neuron2)
+    alignment_objective = (
+        objectives.alignment('mixed4a', decay_ratio=5) +
+        objectives.alignment('mixed4b', decay_ratio=5)
+    )
+    obj = interpolation_objective + 1e-1 * alignment_objective
+    '''
 
     # See more here
     # https://colab.research.google.com/github/tensorflow/lucid/blob/master/notebooks/differentiable-parameterizations/aligned_interpolation.ipynb#scrollTo=jOCYDhRrnPjp
@@ -79,17 +84,15 @@ def render_set(n, channel, vec, train_n):
     
 
 print("Training starting channel")
+
 channel, cn = 'mixed4a_3x3_pre_relu', 25
-images = render_set(cn, channel, Y[0], starting_training_steps)
-
-print (images[0] - images[1])
-print (np.abs(images[0] - images[1]).sum())
-
+images = render_set(cn, channel, starting_training_steps)
 
 for k, img in enumerate(images):
     f_img = os.path.join(save_image_dest, channel + f"_{cn}_{k:06d}.png")
-    imsave(f_img, img)
-    
+    imsave(f_img, img)    
+
+
 
 
 #f_image = 'demo.png'
