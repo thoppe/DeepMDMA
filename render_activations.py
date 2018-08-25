ce9fc71e9d072216058be1d8137f571c705f5156
Travis Hoppe <travis.hoppe@gmail.com>
2018-08-24 23:49:05 -0400

    Non working version

--- render_activations.py
+++ render_activations.py
@@ -6,10 +6,11 @@ Usage:
 Options:
   channel               Specify a channel (default, all channels)
   k                     Specify a color in the channel (default, all valid)
-  --n_training=<n>      Number of training steps [default: 256]
+  --n_training=<n>      Number of training steps [default: 512]
   -o --output_image_size=<n>      Square size of image [default: 600]
   --model_size=<n>      Size of the model CCN, don't change? [default: 200]
   -h --help             Show this screen.
+  -f --force            Force the render
 """
 
 import numpy as np
@@ -52,8 +53,12 @@ def render_set(n, channel):
         optimizer=optimizer, 
     )
     '''
+    ####################################################################
 
     #### NEED TO MAP THE T_IMAGES TOGETHER
+    w, h = int(dargs["--model_size"]), int(dargs["--model_size"])
+    t_size = w
+    
     t_image = make_t_image(lambda:cppn(t_size))
     objective_f = objectives.as_objective(obj)
     optimizer = tf.train.AdamOptimizer(0.005)
@@ -63,23 +68,44 @@ def render_set(n, channel):
     init_global_step = tf.variables_initializer([global_step])
     init_global_step.run()
 
-
+    #####################################################################
     T = import_model(model, transform_f(t_image), t_image)
+    
+    #t_image = image(h, w, decorrelate=True, fft=True, alpha=True)
+    t_rgb = t_image[...,:3]
+    t_alpha = t_image[...,3:]
+    t_bg = image_sample([1, w, h, 3], sd=0.2, decay_power=1.5)
+    print(t_bg, t_alpha, objectives.as_objective(obj)(T))
+    exit()
 
-    obj2 = objectives.as_objective(obj)(T)
+    t_composed = t_bg*(1.0-t_alpha) + t_rgb*t_alpha
+    t_composed = tf.concat([t_composed, t_alpha], -1)
+    t_crop = transform.random_scale([0.6, 0.7, 0.8, 0.9, 1.0, 1.1])(t_composed)
+    t_crop = tf.random_crop(t_crop, [1, 110, 110, 4])
+    t_crop_rgb, t_crop_alpha = t_crop[..., :3], t_crop[..., 3:]
+    t_crop_rgb = linear2gamma(t_crop_rgb)
+    model.import_graph(t_crop_rgb, scope="alpha")
+
+    t_alpha_mean_crop = tf.reduce_mean(t_crop_alpha)
+    t_alpha_mean_full = tf.reduce_mean(t_alpha)
+    exit()
+
 
     #L1 = objective_f*((1.0-t_alpha_mean_full)*0.5)
     #L2 = obj*(1.0-t_alpha_mean_crop)
 
-    #tf.losses.add_loss(-obj*(1.0-t_alpha_mean_full)*0.5)
-    tf.losses.add_loss(-objective_f(T)*(1.0-t_alpha_mean_crop))
-    
-    #tf.losses.add_loss(-objective_f(T))
+    #tf.losses.add_loss(-objective_f(T)*(1.0-t_alpha_mean_full)*0.5)
+    #tf.losses.add_loss(-objective_f(T)*(1.0-t_alpha_mean_crop))
+
+    obj2 = objectives.as_objective(obj)(T)
+    tf.losses.add_loss(-obj2*(1.0-t_alpha_mean_full)*0.5)
+    tf.losses.add_loss(-obj2*(1.0-t_alpha_mean_crop))
+    tf.losses.add_loss(-objective_f(T))
     
     t_loss = tf.losses.get_total_loss()
-
+    print(t_loss)
+    
     vis_op = optimizer.minimize(t_loss, global_step=global_step)
-    #vis_op = optimizer.minimize(-LX, global_step=global_step)
 
     
     tf.global_variables_initializer().run()
@@ -87,7 +113,7 @@ def render_set(n, channel):
         tf.GraphKeys.TRAINABLE_VARIABLES)
 
 
-    if not os.path.exists(f_model):
+    if not os.path.exists(f_model) or is_force:
 
         for i in tqdm(range(training_steps)):
             
@@ -135,22 +161,6 @@ def linear2gamma(a):
 def gamma2linear(a):
   return a**2.2
 
-w, h = int(dargs["--model_size"]), int(dargs["--model_size"])
-t_image = image(h, w, decorrelate=True, fft=True, alpha=True)
-t_rgb = t_image[...,:3]
-t_alpha = t_image[...,3:]
-t_bg = image_sample([1, h, w, 3], sd=0.2, decay_power=1.5)
-  
-t_composed = t_bg*(1.0-t_alpha) + t_rgb*t_alpha
-t_composed = tf.concat([t_composed, t_alpha], -1)
-t_crop = transform.random_scale([0.6, 0.7, 0.8, 0.9, 1.0, 1.1])(t_composed)
-t_crop = tf.random_crop(t_crop, [1, 160, 160, 4])
-t_crop_rgb, t_crop_alpha = t_crop[..., :3], t_crop[..., 3:]
-t_crop_rgb = linear2gamma(t_crop_rgb)
-model.import_graph(t_crop_rgb)
-
-t_alpha_mean_crop = tf.reduce_mean(t_crop_alpha)
-t_alpha_mean_full = tf.reduce_mean(t_alpha)
 
 
 save_image_dest = "results/images"
@@ -174,12 +184,13 @@ if not dargs["<k>"]:
 else:
     COLORSET = [int(dargs["<k>"]),]
 
-
+is_force = dargs["--force"]
+    
 for channel in CHANNELS:
     for n in COLORSET:
 
         f_image = os.path.join(save_image_dest, channel + f"_{n}.jpg")
-        if os.path.exists(f_image):
+        if os.path.exists(f_image) and not is_force:
             continue
         print("Starting", f_image)
         #try:
